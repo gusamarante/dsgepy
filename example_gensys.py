badb708fe4f00bfc10c7f6ef040e14a4188c4720
Gustavo Amarante <gusamarante@gmail.com>
2019-07-30 16:16:11 -0200

    Finished gensys functionality

--- example_gensys.py
+++ example_gensys.py
@@ -1,7 +1,7 @@
 from gensys import gensys
 import numpy as np
 
-g0 = np.array([[1, 0, 0, 0],
+g0 = np.array([[   1, 0, 0, 0],
                [   0, 1, 0, 0],
                [-1.1, 0, 1, 1],
                [   0, 1, 0, 0]])
@@ -11,9 +11,9 @@ g1 = np.array([[0,   0, 1, 0],
                [0,   0, 0, 0],
                [0, 0.7, 0, 0]])
 
-c = np.zeros((5, 1))
+c = np.zeros((4, 1))
 
-psi = np.array([[0, 0],
+psi = np.array([[0,  0],
                 [0,  0],
                 [4,  1],
                 [3, -2]])
@@ -23,4 +23,6 @@ pi = np.array([[1, 0],
                [0, 0],
                [0, 0]])
 
-gensys(g0, g1, c, psi, pi)
+G1, C, impact, fmat, fwt, ywt, gev, eu, loose = gensys(g0, g1, c, psi, pi)
+
+a = 1
--- gensys.py
+++ gensys.py
@@ -1,12 +1,17 @@
+"""
+This code is a translation from matlab to python of Christopher Sim's 'gensys'.
+https://dge.repec.org/codes/sims/linre3a/
+"""
+
 from scipy.linalg import qz
-from numpy.linalg import svd
-from numpy import diagonal, vstack, array, eye, where
+from numpy.linalg import svd, inv
+from numpy import diagonal, vstack, array, eye, where, diag, sqrt, hstack, zeros, arange
 
 
 def gensys(g0, g1, c, psi, pi, div=None, realsmall=0.000001):
 
     # TODO Assert variable types
-
+    unique = False
     eu = [0, 0]
     nunstab = 0
     zxz = False
@@ -42,16 +47,17 @@ def gensys(g0, g1, c, psi, pi, div=None, realsmall=0.000001):
         eu = [-2, -2]
         return None, None, None, None, None, None, None, eu, None
 
-    q1 = q[:n - nunstab + 1, :]
-    q2 = q[n - nunstab + 1:, :]
-    z1 = z[:, :n - nunstab + 1].T
-    z2 = z[:, n - nunstab + 1:]
-    a2 = a[n - nunstab + 1:, n - nunstab + 1:]
-    b2 = a[n - nunstab + 1:, n - nunstab + 1:]
+    q1 = q[:n - nunstab, :]
+    q2 = q[n - nunstab:, :]
+    z1 = z[:, :n - nunstab].T
+    z2 = z[:, n - nunstab:]
+    a2 = a[n - nunstab:, n - nunstab:]
+    b2 = a[n - nunstab:, n - nunstab:]
 
     etawt = q2 @ pi
     neta = pi.shape[1]
 
+    # Case for no stable roots
     if nunstab == 0:
         etawt = None
         ueta = None
@@ -60,20 +66,73 @@ def gensys(g0, g1, c, psi, pi, div=None, realsmall=0.000001):
         bigev = 0
     else:
         ueta, deta, veta = svd(etawt)
+        deta = diag(deta)
         md = min(deta.shape)
-
-        if len(deta.shape) == 1:
-            deta = deta.reshape((max(1, deta.shape[0]), max(1, deta.shape[0])))
-
-        bigev = where(diagonal(deta[:md, :md]) > realsmall)
+        bigev = where(diagonal(deta[:md, :md]) > realsmall)[0]
         ueta = ueta[:, bigev]
         veta = veta[:, bigev]
         deta = deta[bigev, bigev]
+        if deta.ndim == 1:
+            deta = diag(deta)
 
     if len(bigev) >= nunstab:
         eu[0] = 1
 
-    # PAREI NA LINHA 121 NO GENSYS
+    # Case for all stable roots
+    if nunstab == n:
+        etawt1 = None
+        ueta1 = None
+        deta1 = None
+        veta1 = None
+        bigev = 0
+    else:
+        etawt1 = q1 @ pi
+        ndeta1 = min(n - nunstab, neta)
+        ueta1, deta1, veta1 = svd(etawt1)
+        deta1 = diag(deta1)
+        md = min(deta1.shape)
+        bigev = where(diagonal(deta1[:md, :md]) > realsmall)[0]
+        ueta1 = ueta1[:, bigev]
+        veta1 = veta1[:, bigev]
+        deta1 = deta1[bigev, bigev]
+        if deta1.ndim == 1:
+            deta1 = diag(deta1)
+
+    if veta1 is None:
+        unique = True
+    else:
+        loose = veta1 - veta @ veta.T @ veta1
+        ul, dl, vl = svd(loose)
+        if dl.ndim == 1:
+            dl = diag(dl)
+        nloose = sum(abs(diagonal(dl))) > realsmall*n
+        if not nloose:
+            unique = True
+
+    if unique:
+        eu[1] = 1
+    else:
+        print(f'Indeterminacy. {nloose} endogenous variables.')
+
+    tmat = hstack((eye(n - nunstab), -(ueta @ (inv(deta) @ veta.T) @ veta1.T @ deta1 @ ueta1.T).T))
+    G0 = vstack((tmat @ a, hstack((zeros((nunstab, n - nunstab)), eye(nunstab)))))
+    G1 = vstack((tmat @ b, zeros((nunstab, n))))
+
+    G0I = inv(G0)
+    G1 = G0I @ G1
+    usix = arange(n - nunstab, n)
+    C = vstack((tmat @ q @ c, inv(a[usix, :][:, usix] - b[usix, :][:, usix]) @ q2 @ c))
+    impact = G0I @ vstack((tmat @ q @ psi, zeros((nunstab, psi.shape[1]))))
+    fmat = inv(b[usix, :][:, usix]) @ a[usix, :][:, usix]
+    fwt = -inv(b[usix, :][:, usix]) @ q2 @ psi
+    ywt = G0I[:, usix]
+
+    loose = G0I @ vstack((etawt1 @ (eye(neta) - veta @ veta.T), zeros((nunstab, neta))))
+    G1 = (z @ G1 @ z.T).real
+    C = (z @ C).real
+    impact = (z @ impact).real
+    loose = (z @ loose).real
+    ywt = z @ ywt
 
     return G1, C, impact, fmat, fwt, ywt, gev, eu, loose
 
@@ -89,7 +148,7 @@ def qzdiv(stake, A, B, Q, Z, v=None):
 
     for i in reversed(range(n)):
         m = None
-        for j in reversed(range(n)):
+        for j in reversed(range(0, i+1)):
             if (root[j, 1] > stake) or (root[j, 1] < -0.1):
                 m = j
                 break
@@ -103,7 +162,7 @@ def qzdiv(stake, A, B, Q, Z, v=None):
             root[k, 1] = root[k+1, 1]
             root[k + 1, 1] = temp
 
-            if not(v is None):
+            if not (v is None):
                 temp = v[:, k]
                 v[:, k] = v[:, k+1]
                 v[:, k + 1] = temp
@@ -113,13 +172,14 @@ def qzdiv(stake, A, B, Q, Z, v=None):
 
 def qzswitch(i, A, B, Q, Z):
 
-    eps = 1.0e-15
+    eps = 2.2204e-16
+    realsmall = sqrt(eps)*10
 
     a, b, c = A[i, i], A[i, i + 1], A[i + 1, i + 1]
     d, e, f = B[i, i], B[i, i + 1], B[i + 1, i + 1]
 
-    if (abs(c) < eps) and (abs(f) < eps):
-        if abs(a) < eps:
+    if (abs(c) < realsmall) and (abs(f) < realsmall):
+        if abs(a) < realsmall:
             # l.r. coincident zeros with u.l. of A=0. Do Nothing
             return A, B, Q, Z
         else:
@@ -128,8 +188,8 @@ def qzswitch(i, A, B, Q, Z):
             wz = wz / ((wz.T @ wz) ** 0.5)
             wz = array([[wz[0][0],  wz[1][0]], [wz[1][0], -wz[0][0]]])
             xy = eye(2)
-    elif (abs(a) < eps) and (abs(d) < eps):
-        if abs(c) < eps:
+    elif (abs(a) < realsmall) and (abs(d) < realsmall):
+        if abs(c) < realsmall:
             # u.l. coincident zeros with u.l. of A=0. Do Nothing
             return A, B, Q, Z
         else:
@@ -152,7 +212,7 @@ def qzswitch(i, A, B, Q, Z):
         wz = wz / n
         xy = xy / m
         wz = array([[wz[0], wz[1]], [-wz[1], wz[0]]])
-        xy = array([[xy[0], xy[0]], [-xy[1], xy[0]]])
+        xy = array([[xy[0], xy[1]], [-xy[1], xy[0]]])
 
     A[i:i + 2, :] = xy @ A[i:i + 2, :]
     B[i:i + 2, :] = xy @ B[i:i + 2, :]

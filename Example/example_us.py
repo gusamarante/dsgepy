9ce03a33d0e522f0edea4fddd2727b4f6e0a2f77
gusamarante <Jmba@160959>
2022-01-16 00:19:18 -0300

    Minor adjustments to the historical decomposition methods

--- Example/example_us.py
+++ Example/example_us.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pydsge import DSGE
 import matplotlib.pyplot as plt
 from sympy import Matrix, symbols
+from statsmodels.tsa.filters.hp_filter import hpfilter
 
 from pydsge import FRED
 
@@ -18,77 +19,96 @@ df = fred.fetch(series_id=series_dict)
 
 # Observed varibles
 df_obs = pd.DataFrame()
-# df_obs['CPI'] = df['CPI'].dropna().resample('Q').last().pct_change(1) * 4
-# df_obs['FFR'] = df['Fed Funds Rate'].resample('Q').mean()
+df_obs['CPI'] = df['CPI'].dropna().resample('Q').last().pct_change(1) * 4
+df_obs['FFR'] = df['Fed Funds Rate'].resample('Q').mean()
 df_obs['logGDP'] = np.log(df['GDP'].resample('Q').last().dropna())
+df_obs['outputgap'], _ = hpfilter(df_obs['logGDP'].dropna(), 1600)
 
 # ================================
 # ===== MODEL ESPECIFICATION =====
 # ================================
 # endogenous variables at t
-y, yp, g, gm1, mu = symbols('y, yp, g, gm1, mu')
-endog = Matrix([y, yp, g, gm1, mu])
+y, pi, i, a, v, exp_y, exp_pi = symbols('y, pi, i, a, v, exp_y, exp_pi')
+endog = Matrix([y, pi, i, a, v, exp_y, exp_pi])
 
 # endogenous variables at t - 1
-yl, ypl, gl, gm1l, mul = symbols('yl, ypl, gl, gm1l, mul')
-endogl = Matrix([yl, ypl, gl, gm1l, mul])
+yl, pil, il, al, vl, exp_yl, exp_pil = symbols('yl, pil, il, al, vl, exp_yl, exp_pil')
+endogl = Matrix([yl, pil, il, al, vl, exp_yl, exp_pil])
 
 # exogenous shocks
-eps_p, eps_g, eps_mu = symbols('eps_p, eps_g, eps_mu')
-exog = Matrix([eps_p, eps_g, eps_mu])
-
-# parameters
-sigma_p, sigma_mu, phi1, phi2, sigma_g = symbols('sigma_p, sigma_mu, phi1, phi2, sigma_g')
+eps_a, eps_v, eps_pi = symbols('eps_a, eps_v, eps_pi')
+exog = Matrix([eps_a, eps_v, eps_pi])
 
 # expectational shocks
 eta_y, eta_pi = symbols('eta_y, eta_pi')
 expec = Matrix([eta_y, eta_pi])
 
-# model (state) equations
-eq1 = y - yp - g
-eq2 = yp - mu - ypl - sigma_p * eps_p
-eq3 = mu - mul - sigma_mu * eps_mu
-eq4 = g - phi1 * gl - phi2 * gm1l - sigma_g * eps_g
-eq5 = gm1 - gm1l
+# parameters
+sigma, varphi, alpha, beta, theta, phi_pi, phi_y, rho_a, sigma_a, rho_v, sigma_v, sigma_pi = \
+    symbols('sigma, varphi, alpha, beta, theta, phi_pi, phi_y, rho_a, sigma_a, rho_v, sigma_v, sigma_pi')
 
-equations = Matrix([eq1, eq2, eq3, eq4, eq5])
+# Summary parameters
+psi_nya = (1 + varphi) / (sigma * (1 - alpha) + varphi + alpha)
+kappa = (1 - theta) * (1 - theta * beta) * (sigma * (1 - alpha) + varphi + alpha)
+
+# model (state) equations
+eq1 = y - exp_y + (1/sigma)*(i - exp_pi) - psi_nya * (rho_a - 1) * a
+eq2 = pi - beta * exp_pi - kappa * y - sigma_pi * eps_pi
+eq3 = i - phi_pi * pi - phi_y * y - v
+eq4 = a - rho_a * al - sigma_a * eps_a
+eq5 = v - rho_v * vl - sigma_v * eps_v
+eq6 = y - exp_yl - eta_y
+eq7 = pi - exp_pil - eta_pi
+
+equations = Matrix([eq1, eq2, eq3, eq4, eq5, eq6, eq7])
 
 # observation equations
 obs01 = y
-obs_equations = Matrix([obs01])
-obs_names = ['logGDP']
+obs02 = pi
+obs03 = 1/beta - 1 + i
+
+obs_names = ['Output Gap', 'Inflation', 'Interest Rate']
+
+obs_equations = Matrix([obs01, obs02, obs03])
 
 # =============================
 # ===== MODEL ESTIMATION  =====
 # =============================
-estimate_param = Matrix([sigma_p, sigma_mu, phi1, phi2, sigma_g])
+# Not all parameters need to be estimated, these are going to be calibrated
+calib_param = {varphi: 1, alpha: 0.4, beta: 0.9950615}
+estimate_param = Matrix([sigma, theta, phi_pi, phi_y, rho_a, sigma_a, rho_v, sigma_v, sigma_pi])
 
 # priors
-prior_dict = {sigma_p:  {'dist': 'invgamma', 'mean':  0.50, 'std': 0.25, 'label': r'$\sigma_{p}$'},
-              sigma_mu: {'dist': 'invgamma', 'mean':  0.50, 'std': 0.25, 'label': r'$\sigma_{\mu}$'},
-              sigma_g:  {'dist': 'invgamma', 'mean':  0.50, 'std': 0.25, 'label': r'$\sigma_{\mu}$'},
-              phi1:     {'dist': 'normal', 'mean':  0.9, 'std': 0.3, 'label': r'$\phi_{1}$'},
-              phi2:     {'dist': 'normal', 'mean':  0.9, 'std': 0.3, 'label': r'$\phi_{1}$'}}
+prior_dict = {sigma:    {'dist': 'normal',   'mean':  1.30, 'std': 0.20, 'label': '$\\sigma$'},
+              theta:    {'dist': 'beta',     'mean':  0.60, 'std': 0.20, 'label': '$\\theta$'},
+              phi_pi:   {'dist': 'normal',   'mean':  1.50, 'std': 0.35, 'label': '$\\phi_{\\pi}$'},
+              phi_y:    {'dist': 'gamma',    'mean':  0.25, 'std': 0.10, 'label': '$\\phi_{y}$'},
+              rho_a:    {'dist': 'beta',     'mean':  0.50, 'std': 0.25, 'label': '$\\rho_a$'},
+              sigma_a:  {'dist': 'invgamma', 'mean':  0.50, 'std': 0.25, 'label': '$\\sigma_a$'},
+              rho_v:    {'dist': 'beta',     'mean':  0.50, 'std': 0.25, 'label': '$\\rho_v$'},
+              sigma_v:  {'dist': 'invgamma', 'mean':  0.50, 'std': 0.25, 'label': '$\\sigma_v$'},
+              sigma_pi: {'dist': 'invgamma', 'mean':  0.50, 'std': 0.25, 'label': '$\\sigma_{\\pi}$'}}
 
-dsge = DSGE(endog=endog,
-            endogl=endogl,
-            exog=exog,
-            expec=expec,
-            state_equations=equations,
+
+dsge = DSGE(endog, endogl, exog, expec, equations,
             estimate_params=estimate_param,
+            calib_dict=calib_param,
             obs_equations=obs_equations,
             prior_dict=prior_dict,
             obs_data=df_obs,
             obs_names=obs_names,
             verbose=True)
 
-dsge.estimate(nsim=100, ck=0.1, file_path='example_us.h5')
-dsge.eval_chains(burnin=0.1, show_charts=False)
+dsge.estimate(nsim=100, ck=0.1, file_path='snkm.h5')
+dsge.eval_chains(burnin=0.1, show_charts=True)
 
-# print(dsge.posterior_table)
+print(dsge.posterior_table)
+
+# IRFs from the estimated Model
+dsge.irf(periods=24, show_charts=True)
 
 # Extraxct state variables
 df_states_hat, df_states_se = dsge.states()
 
-a = 1
-
+# Historical Decomposition
+df_hd = dsge.hist_decomp(show_charts=True)
